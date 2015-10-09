6526179f5f911ae2513f3a0a9d4680354ce24701
BreanaMarie <msbremarie@gmail.com>
2015-10-09 14:48:46 -0700

    ideas of how the js will behave to meet my goals

--- base.js
+++ base.js
@@ -2,6 +2,13 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+//create on thumbnail click full image appears in display to left
+
+	var image='';
+
+$('#photodisplay').html("<img src=" + image + ">");
+
+//create on submit function on form, text drops below as done
+// create a new form upon submit that will allow for comments
 
 });
