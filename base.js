3f38fac6692d50b9e90d75826ffcf2d277e7ae04
BreanaMarie <msbremarie@gmail.com>
2015-10-12 12:57:47 -0700

    randomize the imagedisplay on window load

--- base.js
+++ base.js
@@ -2,6 +2,60 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+//Start with a random inspiration photo
+window.onload= function (){
+ 	var rand = Math.round((Math.random() * 15) + 1);
+ 	console.log(rand);
+	switch(rand){
+		case 1:
+		$('#photodisplay img').attr('src' , 'images/road1.jpg');
+		break;
+		case 2:
+		$('#photodisplay img').attr('src' , 'images/road2.jpg');
+		break;
+		case 3:
+		$('#photodisplay img').attr('src' , 'images/road3.jpg');
+		break;
+		case 4:
+		$('#photodisplay img').attr('src' , 'images/road4.jpg');
+		break;
+		case 5:
+		$('#photodisplay img').attr('src' , 'images/road5.jpg');
+		break;
+		case 6:
+		$('#photodisplay img').attr('src' , 'images/road6.jpg');
+		break;
+		case 7:
+		$('#photodisplay img').attr('src' , 'images/road7.jpg');
+		break;
+		case 8:
+		$('#photodisplay img').attr('src' , 'images/road8.jpg');
+		break;
+		case 9:
+		$('#photodisplay img').attr('src' , 'images/road9.jpg');
+		break;
+		case 10:
+		$('#photodisplay img').attr('src' , 'images/road10.jpg');
+		break;
+		case 11:
+		$('#photodisplay img').attr('src' , 'images/road11.jpg');
+		break;
+		case 12:
+		$('#photodisplay img').attr('src' , 'images/road12.jpg');
+		break;
+		case 13:
+		$('#photodisplay img').attr('src' , 'images/road13.jpg');
+		break;
+		case 14:
+		$('#photodisplay img').attr('src' , 'images/road14.jpg');
+		break;
+		case 15:
+		$('#photodisplay img').attr('src' , 'images/road15.jpg');
+		break;
+	}
+
+};
+
 //create on thumbnail click full image appears in display to left
 var image='';
 
@@ -9,7 +63,6 @@ $("#photoselector img").on('click', function(e){
 	e.preventDefault();
 	console.log(this);
 	image= $(this).attr('src');
-
 
 //$('#photodisplay').html(image);
 
@@ -35,7 +88,49 @@ $('#timeSubmit').attr('value', dateTime);
 //image selected and pushes the information into corilating tabs
 switch(image){
 	case 'images/road1.jpg':
-	$('#inspiphoto1').append(submission + dateTime);
+	$('#inspiOneSubmits').append(submission + dateTime);
+	break;
+	case 'images/road2.jpg':
+	$('#inspiphoto2').append(submission + dateTime);
+	break;
+	case 'images/road3.jpg':
+	$('#inspiphoto3').append(submission + dateTime);
+	break;
+	case 'images/road4.jpg':
+	$('#inspiphoto4').append(submission + dateTime);
+	break;
+	case 'images/road5.jpg':
+	$('#inspiphoto5').append(submission + dateTime);
+	break;
+	case 'images/road6.jpg':
+	$('#inspiphoto6').append(submission + dateTime);
+	break;
+	case 'images/road7.jpg':
+	$('#inspiphoto7').append(submission + dateTime);
+	break;
+	case 'images/road8.jpg':
+	$('#inspiphoto8').append(submission + dateTime);
+	break;
+	case 'images/road9.jpg':
+	$('#inspiphoto9').append(submission + dateTime);
+	break;
+	case 'images/road10.jpg':
+	$('#inspiphoto10').append(submission + dateTime);
+	break;
+	case 'images/road11.jpg':
+	$('#inspiphoto11').append(submission + dateTime);
+	break;
+	case 'images/road12.jpg':
+	$('#inspiphoto12').append(submission + dateTime);
+	break;
+	case 'images/road13.jpg':
+	$('#inspiphoto13').append(submission + dateTime);
+	break;
+	case 'images/road14.jpg':
+	$('#inspiphoto14').append(submission + dateTime);
+	break;
+	case 'images/road15.jpg':
+	$('#inspiphoto15').append(submission + dateTime);
 	break;
 }
 
